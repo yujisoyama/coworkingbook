c0ce8e9959adb12c324588d29e854387cf62289b
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-10-14 19:03:58 -0300

    Fixes in responsivity

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -12,7 +12,7 @@ export const Home = () => {
                 <p className="mt-5 text-lg font-open font-semibold text-paragraph">Platform to simplify the booking for desks or meeting rooms in coworking spaces</p>
                 <img src={workGif} alt="gif" className="block w-96 h-96 mt-10 mr-auto ml-auto" />
             </div>
-            <div className="w-[400px] h-[510px] p-8 opacity-95 rounded-[30px] bg-backgroundLight relative  before:content-[''] before:absolute before:bg-secondary before:opacity-10 before:inset-0 before:-rotate-[4deg] before:-z-10 before:rounded-[30px] mobile:mt-10 mobile:mb-[90px]">
+            <div className="w-[400px] h-[510px] p-8 opacity-95 rounded-[30px] bg-backgroundLight relative  before:content-[''] before:absolute before:bg-secondary before:opacity-10 before:inset-0 before:-rotate-[4deg] before:-z-10 before:rounded-[30px] mobile:mt-10 mobile:mb-[90px] mobile:w-[350px] mobile:h-[525px]">
                 <div>
                     <div className="">
                         <BriefcaseMetal className="mr-auto ml-auto mt-2" color="#e8e4e6" size={45} weight={"bold"}/>
@@ -29,13 +29,13 @@ export const Home = () => {
                                 <span><Lock className="inline absolute top-4 left-5" size={24} /></span>
                                 <Input type='password' placeholder="Password" />
                             </div>
-                            <div className="w-40 ml-[11.5rem]">
-                                <a className="font-open font-semibold text-sm hover:underline text-main items-end" href="#">Forgot password?</a>
+                            <div className="w-40 ml-[11.5rem] mobile:ml-[10rem]">
+                                <a className="font-open font-semibold text-sm hover:underline text-main" href="#">Forgot password?</a>
                             </div>
                             <button className="w-36 mx-auto h-12 rounded-full font-extrabold hover:bg-[#FFB340] hover:scale-105 duration-300 from-buttonText bg-highlight" type="submit">Sign In</button>
                         </form>
-                        <div className="mt-5 text-sm text-secondary font-semibold ml-2 font-open ">
-                            Don't have an account? <a className="text-main hover:underline" href="#">Create a free account!</a>
+                        <div className="mt-5 text-sm text-secondary font-semibold ml-2 font-open">
+                            <p className="inline mobile:block mobile:text-center">Don't have an account?</p> <a className="text-main hover:underline mobile:block mobile:w-40 mobile:mx-auto" href="#">Create a free account!</a>
                         </div>
                     </div>
                 </div>
