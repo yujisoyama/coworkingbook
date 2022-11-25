23e2de8a972f282f5babb6aec920e662316fe61a
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-25 20:26:43 -0300

    Add EC2 server address

--- web/src/Api.tsx
+++ web/src/Api.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'http://15.228.128.96:3000/',
 });
 
 // baseURL: 'http://localhost:3000'
+// baseURL: 'http://15.228.128.96:3000/',
