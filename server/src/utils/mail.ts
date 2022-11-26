9f660285da5d37c147c6cadb91251746fd5805c4
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-25 22:05:01 -0300

    Put EC2 Address in emails

--- server/src/utils/mail.ts
+++ server/src/utils/mail.ts
@@ -17,7 +17,7 @@ export const sendAccountConfirmEmail = async (uuid: string, email: string, name:
             from: 'Coworking Book Platform <coworkingbook.platform@gmail.com>',
             to: [email],
             subject: 'Coworking Book - Account Activation',
-            html: "<h2>Coworking Book Platform</h2> <p>Hi, " + name + "</p> <p>Click in the link below to confirm your email address and activate your account</p> <a href='http://localhost:3000/user/active/" + uuid + "' target='_blank'>Activate Account</a>",
+            html: "<h2>Coworking Book Platform</h2> <p>Hi, " + name + "</p> <p>Click in the link below to confirm your email address and activate your account</p> <a href='http://15.228.128.96:3000/user/active/" + uuid + "' target='_blank'>Activate Account</a>",
         });
         console.log(mailSent);
     } catch (err) {
--- web/src/components/Home/HomeInformation.tsx
+++ web/src/components/Home/HomeInformation.tsx
@@ -3,7 +3,7 @@ import workGif from "../../../assets/work.gif"
 export const HomeInformation = () => {
     return (
         <div className="mt-5 w-[40rem] mobileHome:w-[400px]">
-            <h1 className="text-4xl text-highlight font-open font-bold mobileHome:pl-4">Coworking Book</h1>
+            <h1 className="text-4xl text-highlight font-open font-bold mobileHome:pl-4">Coworking Booking</h1>
             <p className="mt-5 text-lg font-open font-semibold text-paragraph mobileHome:pl-4 pr-4">Platform to simplify the booking for desks or meeting rooms in coworking spaces</p>
             <img src={workGif} alt="gif" className="block w-96 h-96 mt-10 mr-auto ml-auto mobileHome:mt-3" />
         </div>
