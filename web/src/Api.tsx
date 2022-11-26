d26b5c833ac45cac906157c85533e919ac89c1b6
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-25 22:43:10 -0300

    put local address to run locally

--- web/src/Api.tsx
+++ web/src/Api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://15.228.128.96:3000',
+    baseURL: 'http://localhost:3000',
 });
 
 // baseURL: 'http://localhost:3000'
