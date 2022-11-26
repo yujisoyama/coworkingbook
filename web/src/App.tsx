e7dcf4e92dc80d2a326ee0c016a822707743966e
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-11-25 22:12:56 -0300

    Fixes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import './styles/main.css'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import { Home } from './pages/Home'
-import { SignUp } from './pages/SignUp'
 import { Dashboard } from './pages/Dashboard'
 
 import { UserProvider } from './context/UserContext'
 import { PrivateRoutes } from './components/PrivateRoutes'
 import { Profile } from './pages/Profile'
+import { SignUp } from './pages/SignUp'
 
 function App() {
   return (
