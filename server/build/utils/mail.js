330269d571fc57c7a1a580de278288851e692d2b
Fabio Yuji Soyama <yuji.soyama@gmail.com>
2022-12-06 10:25:16 -0300

    Fixes IP Address in web and server

--- server/build/utils/mail.js
+++ server/build/utils/mail.js
@@ -21,7 +21,7 @@ const sendAccountConfirmEmail = async (uuid, email, name) => {
             from: 'Coworking Book Platform <coworkingbook.platform@gmail.com>',
             to: [email],
             subject: 'Coworking Book - Account Activation',
-            html: "<h2>Coworking Book Platform</h2> <p>Hi, " + name + "</p> <p>Click in the link below to confirm your email address and activate your account</p> <a href='http://15.228.128.96:3000/user/active/" + uuid + "' target='_blank'>Activate Account</a>",
+            html: "<h2>Coworking Book Platform</h2> <p>Hi, " + name + "</p> <p>Click in the link below to confirm your email address and activate your account</p> <a href='http://15.228.154.83:3001/user/active/" + uuid + "' target='_blank'>Activate Account</a>",
         });
         console.log(mailSent);
     }
--- web/src/Api.tsx
+++ web/src/Api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'http://15.228.128.96:3000',
+    baseURL: 'http://15.228.154.83:3001',
 });
 
 // baseURL: 'http://localhost:3000'
